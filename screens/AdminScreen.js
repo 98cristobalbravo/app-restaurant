8e9a97df5e03764ebd94b022472745bc35b61009
98cristobalbravo <9898cristobal@gmail.com>
2024-04-14 23:31:55 -0400

    Actualización de funcionalidades en AdminScreen
    Se ha agregado normalización de nombres de categorías en la pantalla de administrador (AdminScreen).
    Se ha agregado un ScrollView para permitir el desplazamiento de la lista de categorías cuando hay muchas categorías agregadas.

--- screens/AdminScreen.js
+++ screens/AdminScreen.js
@@ -1,14 +1,113 @@
-// AdminScreen.js
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert, ScrollView } from 'react-native';
+import { getDatabase, ref, onValue, push, remove } from 'firebase/database';
+import firebase from '../config/firebase';
 
 const AdminScreen = ({ navigation }) => {
+  const [nuevaCategoriaInput, setNuevaCategoriaInput] = useState('');
+  const [categorias, setCategorias] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    const db = getDatabase(firebase);
+    const categoriasRef = ref(db, 'categorias');
+
+    // Escuchar cambios en la base de datos para actualizar la lista de categorías
+    onValue(categoriasRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const categoriasArray = Object.entries(data).map(([id, categoria]) => ({
+          id,
+          nombre: categoria.nombre_categoria
+        }));
+        setCategorias(categoriasArray);
+      } else {
+        // Si no hay categorías, establecer categorías como un array vacío
+        setCategorias([]);
+      }
+    });
+
+    // Cleanup de la suscripción cuando el componente se desmonte
+    return () => {
+      setCategorias([]); // Limpiamos la lista de categorías al desmontar el componente
+    };
+  }, []);
+
   const handleGoToRegister = () => {
     navigation.navigate('Registro');
   };
 
   const handleGoToMenu = () => {
     navigation.navigate('CrearMenuScreen');
+  };
+
+  const handleAgregarCategoria = () => {
+    if (!nuevaCategoriaInput) {
+      setErrorMessage('Por favor ingresa una nueva categoría.');
+      return;
+    }
+
+    // Convertir la primera letra a mayúscula y las siguientes letras a minúscula
+    const categoriaNormalizada = nuevaCategoriaInput.trim().charAt(0).toUpperCase() + nuevaCategoriaInput.slice(1).toLowerCase();
+
+    // Verificar si la categoría ya existe en la lista de categorías normalizadas
+    if (categorias.find(categoria => categoria.nombre === categoriaNormalizada)) {
+      setErrorMessage('La categoría ya existe.');
+      return;
+    }
+
+    const db = getDatabase(firebase);
+    const categoriasRef = ref(db, 'categorias');
+
+    const nuevaCategoriaObj = {
+      nombre_categoria: categoriaNormalizada
+    };
+
+    push(categoriasRef, nuevaCategoriaObj)
+      .then(() => {
+        setNuevaCategoriaInput('');
+        setErrorMessage(null);
+      })
+      .catch((error) => {
+        setErrorMessage('Error al agregar la nueva categoría: ' + error.message);
+      });
+  };
+
+  const handleEliminarCategoria = (nombre) => {
+    Alert.alert(
+      'Eliminar Categoría',
+      `¿Estás seguro de que deseas eliminar la categoría "${nombre}"?`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Eliminar',
+          onPress: () => {
+            const db = getDatabase(firebase);
+            const categoriasRef = ref(db, 'categorias');
+            const categoriaAEliminar = categorias.find(categoria => categoria.nombre === nombre);
+            if (!categoriaAEliminar) {
+              setErrorMessage('No se encontró la categoría a eliminar.');
+              return;
+            }
+            const { id } = categoriaAEliminar;
+            const categoriaRef = ref(db, `categorias/${id}`);
+            remove(categoriaRef)
+              .then(() => {
+                // Actualizar el estado local eliminando la categoría eliminada
+                const updatedCategorias = categorias.filter(categoria => categoria.id !== id);
+                setCategorias(updatedCategorias);
+                setErrorMessage(null);
+              })
+              .catch((error) => {
+                setErrorMessage('Error al eliminar la categoría: ' + error.message);
+              });
+          }
+        }
+      ]
+    );
   };
 
   return (
@@ -20,6 +119,35 @@ const AdminScreen = ({ navigation }) => {
       <TouchableOpacity onPress={handleGoToMenu} style={styles.button}>
         <Text style={styles.buttonText}>Agregar Menú</Text>
       </TouchableOpacity>
+      <View style={styles.divider} />
+      <TextInput
+        style={styles.input}
+        placeholder="Ingresa categoría"
+        onChangeText={text => setNuevaCategoriaInput(text)}
+        value={nuevaCategoriaInput}
+      />
+      <TouchableOpacity onPress={handleAgregarCategoria} style={styles.button}>
+        <Text style={styles.buttonText}>Agregar Categoría de Menú</Text>
+      </TouchableOpacity>
+
+      {errorMessage && <Text style={styles.errorMessage}>{errorMessage}</Text>}
+      
+      {/* Lista de categorías */}
+      <Text style={styles.subtitle}>Categorías existentes:</Text>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.categoriesContainer}>
+          {categorias.map(categoria => (
+            <TouchableOpacity
+              key={categoria.id}
+              style={styles.categoryButton}
+              onPress={() => handleEliminarCategoria(categoria.nombre)}
+              onLongPress={() => handleEliminarCategoria(categoria.nombre)}
+            >
+              <Text style={styles.categoryButtonText}>{categoria.nombre}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -34,6 +162,19 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginBottom: 20,
   },
+  subtitle: {
+    fontSize: 18,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
   button: {
     width: '80%',
     backgroundColor: 'blue',
@@ -45,6 +186,37 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#fff',
     fontWeight: 'bold',
+  },
+  errorMessage: {
+    color: 'red',
+    marginTop: 10,
+  },
+  categoriesContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  categoryButton: {
+    backgroundColor: 'lightblue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    margin: 5,
+  },
+  categoryButtonText: {
+    fontSize: 16,
+  },
+  divider: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#999',
+    width: '50%',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  scrollView: {
+    maxHeight: 350, // Limitar la altura para que el ScrollView sea scrollable
+    width: '95%',
   },
 });
 
--- screens/CrearMenuScreen.js
+++ screens/CrearMenuScreen.js
@@ -1,116 +1,57 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { getDatabase, ref, push, onValue, set } from 'firebase/database'; // Importa las funciones necesarias de Firebase
-import firebase from '../config/firebase'; // Importa la configuración de Firebase
-import { Picker } from '@react-native-picker/picker'; // Importa Picker desde @react-native-picker/picker
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
+import { getDatabase, ref, push } from 'firebase/database';
+import firebase from '../config/firebase';
 
-const CrearMenu = () => {
-  const [nombre, setNombre] = useState('');
+const CrearMenuScreen = () => {
+  const [nombrePlato, setNombrePlato] = useState('');
   const [precio, setPrecio] = useState('');
-  const [categoria, setCategoria] = useState('');
-  const [categorias, setCategorias] = useState([]);
-  const [nuevaCategoria, setNuevaCategoria] = useState('');
   const [errorMessage, setErrorMessage] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
 
-  useEffect(() => {
-    // Obtener categorías de la base de datos al cargar el componente
-    const db = getDatabase(firebase);
-    const categoriasRef = ref(db, 'categorias');
-
-    onValue(categoriasRef, (snapshot) => {
-      const data = snapshot.val();
-      if (data) {
-        const categoriasArray = Object.values(data);
-        setCategorias(categoriasArray);
-      }
-    });
-  }, []);
-
   const handleGuardarMenu = () => {
-    // Verifica si se ha seleccionado una categoría
-    if (!categoria) {
-      setErrorMessage('Por favor selecciona una categoría.');
+    if (!nombrePlato || !precio) {
+      setErrorMessage('Por favor completa todos los campos.');
       return;
     }
 
-    const db = getDatabase(firebase); // Obtiene una referencia a la base de datos de Firebase
-    const menuRef = ref(db, 'menu'); // Obtiene una referencia a la ubicación donde se guardarán los menús
-    
-    // Crea un nuevo objeto de menú con el nombre, precio y categoría ingresados
+    const db = getDatabase(firebase);
+    const menuRef = ref(db, 'menu');
+
     const nuevoMenu = {
-      nombre: nombre,
-      precio: precio,
-      categoria: categoria
+      nombre_comida: nombrePlato,
+      precio_comida: precio
     };
 
-    // Guarda el nuevo menú en la base de datos
     push(menuRef, nuevoMenu)
       .then(() => {
-        // Limpiar los campos y mostrar un mensaje de éxito
-        setNombre('');
+        setNombrePlato('');
         setPrecio('');
-        setCategoria('');
-        setSuccessMessage(`Menú ${nombre} con precio ${precio} agregado exitosamente.`);
+        setSuccessMessage(`Menú "${nombrePlato}" con precio "${precio}" agregado exitosamente.`);
         setErrorMessage(null);
       })
       .catch((error) => {
-        // Muestra un mensaje de error si falla la operación
         setErrorMessage('Error al guardar el menú: ' + error.message);
         setSuccessMessage(null);
       });
   };
 
-  const handleAgregarCategoria = () => {
-    if (!nuevaCategoria) {
-      setErrorMessage('Por favor ingresa una nueva categoría.');
-      return;
-    }
-
-    const db = getDatabase(firebase);
-    const categoriasRef = ref(db, 'categorias');
-    set(categoriasRef, nuevaCategoria)
-      .then(() => {
-        setNuevaCategoria('');
-        setErrorMessage(null);
-      })
-      .catch((error) => {
-        setErrorMessage('Error al agregar la nueva categoría: ' + error.message);
-      });
-  };
-
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Agregar Menú</Text>
+      <Text style={styles.title}>Crear Menú</Text>
       <TextInput
         style={styles.input}
-        placeholder="Nombre del elemento"
-        onChangeText={setNombre}
-        value={nombre}
+        placeholder="Nombre del plato"
+        onChangeText={setNombrePlato}
+        value={nombrePlato}
       />
       <TextInput
         style={styles.input}
         placeholder="Precio"
         onChangeText={setPrecio}
         value={precio}
+        keyboardType="numeric"
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Nueva categoría"
-        onChangeText={setNuevaCategoria}
-        value={nuevaCategoria}
-      />
-      <TouchableOpacity onPress={handleAgregarCategoria} style={styles.button}>
-        <Text style={styles.buttonText}>Agregar Categoría</Text>
-      </TouchableOpacity>
-      <Picker
-        selectedValue={categoria}
-        onValueChange={(itemValue, itemIndex) => setCategoria(itemValue)}>
-        <Picker.Item label="Agregar categoría" value="" />
-        {categorias.map(c => (
-          <Picker.Item key={c.id} value={c.id} label={c.nombre} />
-        ))}
-      </Picker>
       <TouchableOpacity onPress={handleGuardarMenu} style={styles.button}>
         <Text style={styles.buttonText}>Guardar Menú</Text>
       </TouchableOpacity>
@@ -160,4 +101,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CrearMenu;
+export default CrearMenuScreen;
